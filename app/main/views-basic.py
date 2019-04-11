464d9b2a3fcdaefeb35a71f66e00fcae2930ed1f
Orko Sinha <os92@cornell.edu>
2019-04-11 17:22:53 -0400

    Add duplicae checks to add routes

--- app/main/views-basic.py
+++ app/main/views-basic.py
@@ -4,69 +4,76 @@ from app.main import main
 
 @main.route('/add/right/<string:c>/<string:s>/<string:d>')
 def add_right():
-    right = Rights(cat=c, subcat=s, desc=d)
-    db.session.add(right)
-    db.session.commit()
+    if Rights.query.filter_by(cat=c, subcat=s, desc=d).first() is None:
+      right = Rights(cat=c, subcat=s, desc=d)
+      db.session.add(right)
+      db.session.commit()
 
 @main.route('/add/ttof/<int:fi>/<int:ti>')
 def add_ttof():
-    tttof = TToF(fid=fi, tid=ti)
-    db.session.add(ttof)
-    db.session.commit()
+    if TreatyToForum.query.filter_by(fid=fi, tid=ti).first() is None:
+      tttof = TreatyToForum(fid=fi, tid=ti)
+      db.session.add(ttof)
+      db.session.commit()
 
 @main.route('/add/forum/<string:n>')
 def add_forum():
-  forum = Forum(name=n)
-  db.session.add(forum)
-  db.session.commit()
+  if Forum.query.filter_by(name=n).first() is None:
+    forum = Forum(name=n)
+    db.session.add(forum)
+    db.session.commit()
 
 @main.route('/add/ttor/<int:ri>/<int:ti>')
 def add_ttor():
-  ttor = TtoR(rid=ri, tid=ti)
-  db.session.add(ttor)
-  db.session.commit()
+  if TreatyToRight.query.filter_by(rid=ri, tid=ti).first() is None:
+    ttor = TreatytoRight(rid=ri, tid=ti)
+    db.session.add(ttor)
+    db.session.commit()
 
 @main.route('/add/treaty/<string:n>/<string:u>')
 def add_treaty():
-  treaty = Treaty(name=n, url=u)
-  db.session.add(treaty)
-  db.session.commit()
+  if Treaty.query.filter_by(name=n, url=u) is None:
+    treaty = Treaty(name=n, url=u)
+    db.session.add(treaty)
+    db.session.commit()
 
 @main.route('/add/ttoc/<int:ci>/<int:ri>/<string:d>')
 def add_ttoc():
-  ttoc = TreatyToCountry(countryid=ci, treatyid=ti, date=d)
-  db.session.add(ttoc)
-  db.commit()
+  if TreatyToCountry.query.filter_by(cid=ci, tid=ti, date=d).first() is None:
+    ttoc = TreatyToCountry(cid=ci, tid=ti, date=d)
+    db.session.add(ttoc)
+    db.commit()
 
 @main.route('/add/country/<string:n>')
 def add_country():
-  country = Country(name= n)
-  db.session.add(country)
-  db.commit()
+  if Country.query.filter_by(name=n).first() is None:
+    country = Country(name= n)
+    db.session.add(country)
+    db.commit()
 
 @main.route('/delete/rights/cat/<string:c>')
 def delete_rights_category():
-  cats = Rights.query.filter_by(cat=c)
-  while cats is not None:
+  cats = Rights.query.filter_by(cat=c).all()
+  while len(cats) is not 0:
     db.session.delete(cats)
     db.session.commit()
-    cats = Rights.query.filter_by(cat=c)
+    cats = Rights.query.filter_by(cat=c).all()
 
 @main.route('delete/rights/subcat/<string:s>')
 def delete_rights_subcategory():
-  subcats = Rights.query.filter_by(subcat=s)
-  while subcats is not None:
+  subcats = Rights.query.filter_by(subcat=s).all()
+  while len(subcats) is not 0:
     db.session.delete(subcats)
     db.session.commit()
-    subcats = Rights.query.filter_by(subcat=s)
+    subcats = Rights.query.filter_by(subcat=s).all()
 
 @main.route('delete/rights/disc/<string:d>')
 def delete_rights_description():
-  descs = Rights.query.filter_by(desc=d)
-  while descs is not None:
+  descs = Rights.query.filter_by(desc=d).all()
+  while len(descs) is not 0:
     db.session.delete(descs)
     db.session.commit()
-    descs = Rights.query.filter_by(desc=d)
+    descs = Rights.query.filter_by(desc=d).all()
 
 @main.route('delete/ttof/fid/<string:f>')
 def delete_ttof_fid():
@@ -123,7 +130,3 @@ def delete_ttoc_tid():
   if ttoc_entry is not None:
     db.session.delete(ttoc_entry)
     db.session.commit()
-
-
-
-
