b607df5a98cdbd01a80683e9fda27022936857bd
riddhima25 <riddhima.narravula@gmail.com>
2019-04-11 11:43:13 -0400

    made changes

--- app/main/views.py
+++ app/main/views.py
@@ -25,10 +25,11 @@ def startForm():
     categories = {}
     subcategories = {}
     discrimination = {}
+    editable_html_obj = EditableHTML.get_editable_html('about')
     for right in rights:
         categories.add(right.cat)
         subcategories.add(right.subcat)
         discrimination.add(right.disc)
     # return render_template('/templates/layouts/index.html', categories=categories,
     #subcategories=subcategories, discrimination=discrimination)
-    return render_template('main/about.html')
+    return render_template('main/about.html', editable_html_obj=editable_html_obj)
