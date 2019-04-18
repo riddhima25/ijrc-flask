ba5c375fd701e72295af007af9b8da1ded29b1bd
Shreeya Gad <shreeyagad@Shreeyas-MacBook-Air.local>
2019-04-17 21:48:13 -0400

    filter routes

--- app/main/views-filter.py
+++ app/main/views-filter.py
@@ -19,7 +19,7 @@ def startForm():
         discrimination.add(right.discrimination)
     # return render_template('/templates/layouts/index.html', categories=categories,
     #subcategories=subcategories, discrimination=discrimination)
-    return render_template('main/about.html')
+    return rights
 
 @main.route('/category/<string:category>')
 def showCategory():
@@ -30,8 +30,9 @@ def showCategory():
     for right in category:
         subcategories.add(right.subcategory)
         discrimination.add(right.discrimination)
-    return render_template('/templates/layouts/index.html', categories=category,
-    subcategories=subcategories, discrimination=discrimination)
+    #return render_template('/templates/layouts/index.html', categories=category,
+    #subcategories=subcategories, discrimination=discrimination)
+    return category;
 
 @main.route('/category/<string:category>/<string:subcategory>')
 def showSubcategory():
@@ -40,23 +41,27 @@ def showSubcategory():
     discrimination = {}
     for right in subcategory:
         discrimination.add(right.discrimination)
-    return render_template('/templates/layouts/index.html', categories=category,
-    subcategories=subcategory.subcategory, discrimination=discrimination)
+    #return render_template('/templates/layouts/index.html', categories=category,
+    #subcategories=subcategory.subcategory, discrimination=discrimination)
+    return subcategory;
 
 @main.route('/category/<string:category>/<string:subcategory>/<string:discrimination>')
 def showDiscrimination():
     discrimination = db.session.query(Right).filter_by(category=category, subcategory=subcategory, discrimination=discrimination).all()
     session['currentDiscrimination'] = discrimination.discrimination
-    return render_template('/templates/layouts/index.html', categories=category,
-    subcategories=subcategory, discrimination=discrimination.discrimination)
+    #return render_template('/templates/layouts/index.html', categories=category,
+    #subcategories=subcategory, discrimination=discrimination.discrimination)
+    return discrimination
 
 @main.route('/category/<string:category>/<string:subcategory>/<string:discrimination>/<string:age>')
 def showAge():
     age = db.session.query(Right).filter_by(category=category, subcategory=subcategory, discrimination=discrimination, age=age).all()
     session['currentAge'] = age.age
-    return render_template('/templates/layouts/index.html', categories=category,
-    subcategories=subcategory, discrimination=discrimination, age=age.age)
+    #return render_template('/templates/layouts/index.html', categories=category,
+    #subcategories=subcategory, discrimination=discrimination, age=age.age)
+    return age
 
 @main.route('/results')
 def showResults():
-    
+    return render_template('/templates/layouts/client_side_results.html', categories=category,
+    #subcategories=subcategory, discrimination=discrimination, age=age.age)
