67818b09b2333e788ee53c4e95ef8fcb80e3f1b6
ekb72 <ekb72@cornell.edu>
2019-05-02 21:07:38 -0400

    changes to date

--- app/main/views.py
+++ app/main/views.py
@@ -328,3 +328,10 @@ def FilterBySubcategory(subcat):
     for entry in rights:
         treaties.append(db.session.query(Treaty).filter_by(id=entry.tid).first())
     return treaties
+
+@main.route('/date/<datetime:date>/<string:subcategory>/<string:category>/<string:discrimination>/<string:forum>'country)
+def FilterTreatybyDate(date):
+    treatyids = db.session.query(TreatyToCountry).filter(TreatyToCountry.date>= date).with_entities(TreatyToCountry.tid)
+    for tid in treatyids:
+        treatylist.append(db.session.query(Treaty).filter_by(Treaty.id = tid)
+    return treaties
